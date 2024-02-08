65a682307bed49edc1b77d00ab3b1f61f938ef9d
Rafael Thomaz <oirafaelthz@gmail.com>
2024-02-08 13:55:58 -0300

    Aula 3

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,85 @@
 import logo from './assets/logo-nlw-expert.svg';
 import { NoteCard } from './components/note-card';
 import { NewNoteCard } from './components/new-note-card';
+import { ChangeEvent, useState } from 'react';
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
 
 export function App() {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray);
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  function toNormalForm(str: string) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
+  const filteredNotes = search !== ''
+    ? notes.filter(
+      note => toNormalForm(note.content.toLocaleLowerCase()).includes(toNormalForm(search.toLocaleLowerCase()))
+    ) : notes
+
   return (
-    <div className="mx-auto max-w-6xl my-16 space-y-6">
+    <div className="mx-auto max-w-6xl my-16 space-y-6 px-5">
       <img src={logo} alt="Logotipo NLW Expert" />
 
-      <form className="w-full">
+      <form className="w-full space-y-3">
         <input
           type="text"
           placeholder='Busque em suas notas'
-          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          className='w-full bg-transparent text-2xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          onChange={handleSearch}
         />
+        <div className='h-px bg-slate-700' />
       </form>
 
-      <div className='h-px bg-slate-700' />
+      <div className='grid sm:md:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard />
-        <NoteCard note={{
-          date: new Date(Date.now()),
-          content: 'Hello, world'
-        }} />
+        {filteredNotes.map(note => {
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+        })}
       </div>
     </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -2,22 +2,27 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { X } from 'lucide-react'
+import { FaTrash } from "react-icons/fa"
+
 
 interface NoteCardProps {
   note: {
-    date: Date,
+    id: string
+    date: Date
     content: string
-  }
+  },
+
+  onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
-        <span className='text-small font-medium text-slate-300'>
+      <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 px-5 pt-5 gap-2 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+        <span className='text-xs font-medium text-slate-300'>
           {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
         </span>
-        <p className='text-small leading-6 text-slate-400'>
+        <p className='text-sm leading-6 text-slate-400 h-full overflow-scroll'>
           {note.content}
         </p>
         <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-b from-black/0 to-black/60 pointer-events-none' />
@@ -25,25 +30,26 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-        <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden'>
+        <Dialog.Content className='fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden'>
           <Dialog.Close className='absolute top-o right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
             < X className='size-5' />
           </Dialog.Close>
 
-          <div className='flex flex-1 flex-col gap-3 p-5'>
-            <span className='text-small font-medium text-slate-300'>
+          <div className='flex flex-1 flex-col gap-3 px-5 pt-5 h-full'>
+            <span className='text-sm font-medium text-slate-300'>
               {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
             </span>
-            <p className='text-small leading-6 text-slate-400'>
+            <p className='text-md leading-6 text-slate-400 overflow-scroll md:h-[calc(100%-96px)] pb-4'>
               {note.content}
             </p>
           </div>
 
           <button
             type='button'
-            className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+            onClick={() => onNoteDeleted(note.id)}
+            className='w-full absolute bottom-0 inline-flex items-center justify-center gap-1.5 bg-slate-800 py-4 text-center text-sm text-red-400 outline-none font-medium hover:underline'
           >
-            Quero <span className='text-red-400 group-hover:underline'>apagar essa nota</span>
+            <FaTrash className='w-3 h-3' /> Apagar essa nota
           </button>
         </Dialog.Content>
       </Dialog.Portal>
